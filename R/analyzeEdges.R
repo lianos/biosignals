c5c3b2d31c6c246407d5dafa60560b9883245b99
Steve Lianoglou <slianoglou@gmail.com>
2011-10-25 23:49:32 -0400

    pruning hedges

--- R/analyzeEdges.R
+++ R/analyzeEdges.R
@@ -1,45 +1,12 @@
-##' Analyzes 1D data using multiscale analysis.
+##' Detects edges across a raw signal vector
 ##'
-##' Finds distinct transition regions and transition ratios.
-##'
-##' @param x The 1D data
-##' @param scales A vector of scales at which to perform analysis
-##' @param threshold A vector of numbers between [0, 1] indicating
-##' how "obvious" a step has to be at each scale in order to be
-##' considered a transition
-##' @param timestep The amount of time one data point represents
-##' @param start.time The time at which to start analysis
-##' @param end.time The time at which to end analysis
-##' @param tran.rad The number of samples to ignore on either side
-##' of a transition point when calculating region statistics
-##'
-##' @return $dData deriviative scale space,
-##' $minmax: a vector that indicates minima and maxima within the data
-##' $stats A table of mean and std.dev for each distinct data region
-analyzeEdges <- function(x, scales=c(1, 2, 4, 8),
-                         thresholds=c(0.1, 0.2, 0.3, 0.4),
-                         timestep=1L, start.time=1L, end.time=length(x),
-                         tran.rad=1) {
-
-  ## Limit the analysis to time region specified?
-  if (missing(timestep) || missing(start.time) || missing(end.time)) {
-    time <- 1:length(x)
-  }
-
-  d.data <- createGaussScaleSpace(x, 1, scales)
-
-  ## Find the position of local min/max at the most coarse scale
-  min.max <- findLocalExtrema(d.data[, ncol(d.data)], tail(thresholds, 1),
-                              tail(scales, 1))
-}
-
-##' Detects edges across a signal vector
+##' This function currently assumes that the values of x are non-negative
 ##'
 ##' Locations where the smoothed second derivative crosses x=0 indicate
 ##' a potential edge.
 ##'
 ##' The (local) magnitude of the peak in the first derivative are used to filter
-##' potential edges that are due to noise ... tweak this using the threshold
+##' potential edges arising from noise ... tweak this using the threshold
 ##' and hte edge.window parameter.
 detectEdges <- function(x, bandwidth=10, mu=0, sd=1, threshold=0.15,
                         edge.window=2*bandwidth, thresh.mult=0.3, ...) {
--- R/localExtrema.R
+++ R/localExtrema.R
@@ -1,22 +1,72 @@
+
+## TODO: Refactor some stuff out of the detectEdges code and put it in here.
+##       Edge detection is just amounting to finding local extrema of the
+##       first derivative of the signal (by using zero-crossings of the 2nd)
+localExtrema <- function(x, window, threshold=0.15,
+                         bandwidth=ceiling(window / 2), mu=0, sd=1,
+                         x0=NULL, x1=NULL, ...) {
+  stop("This doesn't work yet -- almost does for smoothed x")
+  if (is.null(x0)) {
+    x0 <- convolve1d(x, 'normal', bandwidth=bandwidth, mu=mu, sd=sd)
+  }
+  if (is.null(x1)) {
+    x1 <- convolve1d(x, 'normal', bandwidth=bandwidth, mu=mu, sd=sd, deriv=1)
+  }
+
+  x.maxs <- slidingMax(x, as.integer(max(1L, window / 2)))
+  x.mins <- slidingMin(x, as.integer(max(1L, window / 2)))
+  shift.up <- min(x.mins)
+  ## if (shift.up <= 0) {
+  ##   x.maxs <- x.maxs + shift.up + 1L
+  ##   x.mins <- x.mins + shift.up + 1L
+  ##   x1 <- x1 + shift.up + 1L
+  ## }
+
+  ## Locations where derivative cross 0 are where the local extrema
+  ## will be
+  zeroes <- zeroCrossings(x1)
+
+  ## Filter out noisy peaks by assuring value at zerocrossings at x0
+  ## are sufficiently far from their surround, identified by the size
+  ## of the window
+
+  ## ---------------------------------------------------------------------------
+  ## Find maxima
+  ##
+  ## positions where the 2nd derivative switches from positive to negative
+  ## indicate potential maxima
+  maxima <- zeroes[as.logical(x1[zeroes - 1] > 0)]
+  dist2min <- x.maxs[maxima] - x.mins[maxima]
+  mx <- maxima[dist2min / maxima > threshold]
+
+  ## ---------------------------------------------------------------------------
+  ## Find minima
+  minima <- zeroes[as.logical(x1[zeroes - 1]) < 0]
+  dist2max <- x.maxs[minima] - x.mins[minima]
+  mn <- minima[dist2max / minima > threshold]
+
+  list(maxima=mx, minima=mn)
+}
+
 findLocalExterma <- function(x, threshold=0.5, scale=1, regions=NULL) {
-  
+
   xmax <- x / max(x)
   xmin <- x / min(x)
-  
+
   winmax <- numeric(length(x))
   winmin <- numeric(length(x))
-  
+
   for (i in (1+scale):(length(x)-scale)) {
     winmax[i] <- max(winmax[(i-scale):(i+scale)])
     winmin[i] <- max(winmax[(i-scale):(i+scale)])
   }
-  
+
   maxima <- logical(length(x))
   minima <- logical(length(x))
-  
+
   ii <- (1+scale):(length(x)-scale)
   maxima[ii] <- xmax[ii] >= threshold & xmax[ii] >= winmax[ii]
   minima[ii] <- xmin[ii] >= threshold & xmin[ii] >= winmin[ii]
-  
+
   maxima + minima
 }
